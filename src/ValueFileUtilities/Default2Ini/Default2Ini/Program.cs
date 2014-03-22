427fe4719acedc5d2a4319e9f5e607ccb1c093d6
Tomas Kulhanek <tomaton@centrum.cz>
2014-03-22 22:01:18 +0100

    added script to modify HumMod like model based on initial values in file

---
+++ src/ValueFileUtilities/Default2Ini/Default2Ini/Program.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Default2Ini
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (args.Length > 0)
+            {
+                //var myIniParser = new readIniFile(args[0]);
+                if (args[0].Equals("c"))
+                {
+                    if (args.Length > 2) DefaultFile2IniFile(args[1], args[2]);
+                }
+                else if (args[0].Equals("m"))
+                {
+                    if (args.Length > 3) IniFile2ModelFile(args[1], args[2],args[3]);
+                }
+                else Help();
+            }
+            else Help();
+        
+        }
+
+        private static void Help()
+        {
+            Console.WriteLine(
+                "Usage: \nc [defaultfile] [inifile]\n    converts default file to inifile\nm [inifile] [modelfile]\nm     converts inifile to modelica");
+        }
+
+        private static void IniFile2ModelFile(string inifile, string modelfile1, string modelfile2)
+        {
+            //throw new NotImplementedException();
+            var pairs = ReadIniFile(inifile);
+            WriteModel(modelfile1,modelfile2, pairs);
+        }
+
+        private static void WriteModel(string mf1,string mf2, Dictionary<string, string> pairs)
+        {
+         //   throw new NotImplementedException();
+            TextReader defaultFile = null;
+            TextWriter iniFile = null;
+            if (File.Exists(mf1))
+            {
+                try
+                {
+                    defaultFile = new StreamReader(mf1);
+                    iniFile = new StreamWriter(mf2);
+                    var strLine = defaultFile.ReadLine();
+                    while (strLine != null)
+                    {
+                        //strLine = strLine.Trim(); //.ToUpper();
+                        //iniFile.WriteLine(strLine + "=" + defaultFile.ReadLine());
+                        if (CheckVariables(strLine)) strLine = ConvertVariables(strLine, pairs);
+                        else if (CheckConstant(strLine)) strLine = convertConstant(strLine, pairs);
+                        iniFile.WriteLine(strLine);
+                        strLine = defaultFile.ReadLine();
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (iniFile != null)
+                        iniFile.Close();
+                    if (defaultFile != null)
+                        defaultFile.Close();
+                }
+            }
+        }
+
+        private static string convertConstant(string strLine, Dictionary<string, string> pairs)
+        {
+            var regex = new Regex("varName = \"([^\"]*)[^\\)]*");
+            var match = regex.Match(strLine);
+            if (match.Success)
+            {
+                var varValue = pairs[match.Groups[1].Value];//Regex.Find(strLine, regex)];
+                return regex.Replace(strLine, "varName = \"$1\", varValue = \"" + varValue + "\"");
+            }
+            else return strLine;
+        }
+
+        private static string ConvertVariables(string strLine, Dictionary<string, string> pairs)
+        {
+            var regex = new Regex("varName = \"([^\"]*)[^\\)]*");
+            var match = regex.Match(strLine);
+            if (match.Success)
+            {
+                var varValue = pairs[match.Groups[1].Value];//Regex.Find(strLine, regex)];
+                return regex.Replace(strLine, "varName = \"$1\",varValue = \"" + varValue + "\"");
+            }
+            else return strLine;
+            //var varValue = pairs[Regex.Find(strLine,regex)];
+            //return Regex.Replace(strLine, regex,"varname = \"$1\" varValue = \"" + varValue + "\"");
+        }
+
+        private static bool CheckConstant(string strLine)
+        {
+            return Regex.IsMatch(strLine, "[ ]*Physiolibrary\\.Utilities\\.ConstantFromPhysiovalues");
+        }
+
+        private static bool CheckVariables(string strLine)
+        {
+            return Regex.IsMatch(strLine, "[ ]*Variable");
+        }
+
+        /// <summary>
+        /// Opens the INI file at the given path and enumerates the values in the IniParser.
+        /// </summary>
+        /// <param name="iniPath">Full path to INI file.</param>
+        public static Dictionary<string,string> ReadIniFile(String iniPath)
+        {
+            Dictionary<string,string> keyPairs = new Dictionary<string, string>();
+            TextReader iniFile = null;
+            String strLine = null;
+            String[] keyPair = null;
+            if (File.Exists(iniPath))
+            {
+                try
+                {
+                    iniFile = new StreamReader(iniPath);
+
+                    strLine = iniFile.ReadLine();
+
+                    while (strLine != null)
+                    {
+                        strLine = strLine.Trim(); //.ToUpper();
+
+                        if (strLine != "")
+                        {
+                            keyPair = strLine.Split(new char[] {'='}, 2);
+
+                            String value = null;
+
+                            if (keyPair.Length > 1)
+                                value = keyPair[1];
+
+                            keyPairs.Add(keyPair[0], value);
+                        }
+                        strLine = iniFile.ReadLine();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (iniFile != null)
+                        iniFile.Close();
+                }
+            }
+            else
+                throw new FileNotFoundException("Unable to locate " + iniPath);
+            return keyPairs;
+        }
+
+        //read default file odd rows are varnames, even rows are values
+        private static void DefaultFile2IniFile(string arg1, string arg2)
+        {
+            //throw new Exception("The method or operation is not implemented.");
+            TextReader defaultFile = null;
+            TextWriter iniFile = null;
+            if (File.Exists(arg1))
+            {
+                try
+                {
+                    defaultFile = new StreamReader(arg1);
+                    iniFile = new StreamWriter(arg2);
+                    var strLine = defaultFile.ReadLine();
+                    while (strLine != null)
+                    {
+                        //strLine = strLine.Trim(); //.ToUpper();
+                        iniFile.WriteLine(strLine + "=" + defaultFile.ReadLine());
+                        strLine = defaultFile.ReadLine();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    if (iniFile != null)
+                        iniFile.Close();
+                    if (defaultFile != null)
+                        defaultFile.Close();
+                }
+            }
+        }
+
+
+    }
+}
---
+++ test/TestFMuSimulator/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimulatorBalancerLibrary;
+
+namespace TestFMuSimulator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test of FMU simulator library");
+            TestRestFMUSimulator();//string modelName,string updateURL)
+            Console.Write("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static void TestRestFMUSimulator()
+        {
+            var rfmu = new RestFMUSimulator("testModel",null);
+            var pnames = new string[] {"im[1].k_imai[1]", "im[1].k_imai[2]", "im[1].k_imai[3]", "im[1].k_imai[4]"};
+            var pvalues = new double[][] { new double[] { 2, 3, 4, 5 }, new double[] { 1.1, 2.1, 3.1, 4.1 }, new double[] { 1.2, 2.2, 3.2, 4.2 } };
+            var testresults = new double[] {0.7389, 0.7358, 0.7362};
+            var vnames = new string[] {"time","im[1].sO2"};
+            var timepoints = new double[]{0,5,10,15,20,25,30,35,40,45,50,55,60};
+            //http://ipv4.fiddler http://localhost.fiddler
+            string workerurl = "http://localhost.fiddler:48051/worker/simulation/MatejakAB2013_Kulhanek2013.fmu";
+            rfmu.RestURLs = new string[]{workerurl};
+            var results = rfmu.Simulate( pnames,pvalues,vnames,timepoints);
+            if (results.Length!=3) throw new Exception("Test must return 3 arrays of results.");
+            if (results[0].Length!=timepoints.Length) throw new Exception("Each result should have timepoints variables.");
+            if (results[0][0].Length!=vnames.Length) throw new Exception("Each row should have same numbers as variable names.");
+            
+                for (int i = 0; i < results.Length; i++)
+                {
+                    for (int j = 0; j < results[i].Length; j++)
+                    {
+                        for (int k = 0; k < results[i][j].Length; k++)
+                        {
+                            Console.Write(results[i][j][k] + " ");
+                        }
+                        Console.WriteLine();
+                    }
+                    Console.WriteLine("----");
+                }
+                //TODO compare results
+                for (int i = 0; i < testresults.Length; i++)
+                {
+                    if (Math.Abs(results[i][timepoints.Length - 1][1] - testresults[i]) > 1e-4) Console.WriteLine("difference between expected and retrieved result:" + testresults[i]+ " - "+ results[i][timepoints.Length - 1][1] );
+                }
+        }
+    }
+}
